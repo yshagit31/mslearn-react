d9d658e058a34b77b9cca908e8d02f294c5e2c15
yshagit31 <yshashank024@gmail.com>
2024-09-07 19:44:43 +0530

    Commit1

---
+++ code/0-starter/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import RecipeTitle from "./RecipeTitle";
+import './index.css'
+function App()
+{
+    return(
+        <article>
+            <h1>Recipe Manager</h1>
+            <RecipeTitle></RecipeTitle>
+        </article>
+    )
+}
+export default App;
